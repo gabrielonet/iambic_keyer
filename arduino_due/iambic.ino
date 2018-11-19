9857d62e4ea52a07272a97deba7bf5cfd166aecc
gabrielonet <gabriel@linux-romania.com>
2018-11-19 23:08:43 +0200

    Update iambic.ino

--- arduino_due/iambic.ino
+++ arduino_due/iambic.ino
@@ -1,4 +1,5 @@
 int ledPin = 13;   // LED connected to digital pin 13
+int ptt = 12 ;     // PTT output
 int dahPin = 7;    // DAH  digital input pin 7
 int ditPin = 6;    // DIT digital input 6
 int spd = 300 ;    // keyer WPM speed
@@ -6,12 +7,20 @@ int piezoPin = 3;  //  8 ohm speaker  with 330 ohm in series
 int dah = 1 ;
 int dit = 1 ;
 int analogPin = A3;     // potentiometer connected to analog pin 3 , ground and 3.3V (arduino Due !!! other boards  may accept 5 V but be carefull)
+unsigned long previousMillis = millis();
+unsigned long currentMillis = millis();
+
+
 void setup()
 {
   pinMode(ledPin, OUTPUT);              // sets the digital pin 13 as output
   pinMode(dahPin, INPUT_PULLUP);        // sets the digital pin 7 as input and internal resistor to HIGH
   pinMode(ditPin, INPUT_PULLUP);        // sets the digital pin 6 as input and internal resistor to HIGH 
+  pinMode(ptt, OUTPUT) ;
+  digitalWrite(ptt, HIGH);
+   Serial.begin(115200);
 }
+
 
 void loop()
 {
@@ -20,7 +29,9 @@ void loop()
   dah = digitalRead(dahPin);     // read the DAH input pin
   dit = digitalRead(ditPin);     // read the DIT input pin
   
-  if (dah == LOW) { 
+  if (dah == LOW) {
+    previousMillis = millis();
+    digitalWrite(ptt, LOW);
     digitalWrite(ledPin, HIGH);
     analogWrite(3,127);
     delay(spd);
@@ -30,11 +41,20 @@ void loop()
   }
   
   if (dit == LOW) { 
+    previousMillis = millis();
+    digitalWrite(ptt, LOW);
     digitalWrite(ledPin, HIGH);
     analogWrite(3,127);
     delay(spd/3);
     digitalWrite(ledPin, LOW);
     analogWrite(3,0);
     delay(spd/3)  ;
+    
   }
+  currentMillis = millis();
+  if (currentMillis - previousMillis >= spd*3) {
+      digitalWrite(ptt, HIGH);
+      Serial.println(currentMillis - previousMillis);
+  }
+  
 }
