1678b8d64e31d1d1e5898d6b9a5199c22e104288
gabrielonet <gabriel@linux-romania.com>
2019-01-17 21:53:55 +0200

    Update iambic.ino

--- arduino/iambic.ino
+++ arduino/iambic.ino
@@ -1,42 +1,41 @@
 int ledPin = 13;   // LED connected to digital pin 13
-int key_in = 12;     // Straight Key input
+int key_in = 12;     // Straight Key input pin 12
 int straight = 0 ; // straight key temp variable
-int ptt_in = 11 ;    // PTT input
-int dahPin = 10;    // DAH  digital input pin 7
-int ditPin = 9;    // DIT digital input 6
-int ptt_out = 8 ;     // PTT output
-int key_out = 7 ;     // key output
-int piezoPin = 5;  //  8 ohm speaker  with 330 ohm in series
+int ptt_in = 11 ;    // PTT input pin 11
+int dahPin = 10;    // DAH  digital input pin 10
+int ditPin = 9;    // DIT digital input 9
+int ptt_out = 8 ;     // PTT output pin 8
+int key_out = 7 ;     // key output pin 7
+int piezoPin = 5;  //  8 ohm speaker  with 330 ohm in series pin 5
 int spd = 300 ;    // keyer WPM speed
 int dah = 1 ;
 int dit = 1 ;
 int val = 0;     // variable for reading straight key
-int analogPin = A7;     // potentiometer connected to analog pin 3 , ground and 3.3V (arduino Due !!! other boards  may accept 5 V but be carefull)
+int analogPin = A7;     // potentiometer connected to analog pin 11 , ground and 3.3V (arduino Due !!! other boards  may accept 5 V but be carefull)
 unsigned long previousMillis = millis();
 unsigned long currentMillis = millis();
 void setup()
 {
   pinMode(ledPin, OUTPUT);              // sets the digital pin 13 as output
-  pinMode(dahPin, INPUT_PULLUP);        // sets the digital pin 7 as input and internal resistor to HIGH
-  pinMode(ditPin, INPUT_PULLUP);        // sets the digital pin 6 as input and internal resistor to HIGH 
+  pinMode(dahPin, INPUT_PULLUP);        // sets the dah pin as input and internal resistor to HIGH
+  pinMode(ditPin, INPUT_PULLUP);        // sets the dit pin as input and internal resistor to HIGH 
   pinMode(ptt_out, OUTPUT) ;
   pinMode(ptt_in, INPUT_PULLUP) ;
   pinMode(key_out, OUTPUT);
   pinMode(key_in, INPUT_PULLUP);  
   digitalWrite(ptt_out, HIGH);
   digitalWrite(key_out, HIGH);
-  Serial.begin(9600);
 }
 
 void loop()
 {
-  spd = analogRead(analogPin);   // read potentiometer analogue value
-  dah = digitalRead(dahPin);     // read the DAH input pin
-  dit = digitalRead(ditPin);     // read the DIT input pin
-  val = digitalRead(key_in);  // read input value
+  spd = analogRead(analogPin);  // read potentiometer analogue value
+  dah = digitalRead(dahPin);    // read the DAH input pin
+  dit = digitalRead(ditPin);    // read the DIT input pin
+  val = digitalRead(key_in);    // read straight key input pin
 
-  
-  if (val == LOW )      // check if the input is HIGH (straight key released)
+  // Begin straight key 
+  if (val == LOW )      // check if straight key pressed
   {
     previousMillis = millis();
     straight = 1 ;
@@ -45,21 +44,16 @@ void loop()
     digitalWrite(key_out, HIGH);
     analogWrite(piezoPin,127);
   } 
-  if (val == HIGH && straight == 1)
+  if (val == HIGH && straight == 1) // check if straight key is released
     {
       //previousMillis = millis();
       straight == 0 ;
       digitalWrite(ledPin, LOW);  // turn LED OFF
       analogWrite(piezoPin,0);
       digitalWrite(key_out, LOW);
-
-          
     }  
-
-
-
-
     
+    // Begin iambic key  //
   if (dah == LOW) {
     previousMillis = millis();
     digitalWrite(ptt_out, LOW);
@@ -86,12 +80,12 @@ void loop()
     delay(spd/3)  ;
     
   }
-  currentMillis = millis();
   
+ // Begin computing delta for Full Break  PTT hang time
+  currentMillis = millis();
   if (currentMillis - previousMillis >= spd*3)
   {
       digitalWrite(ptt_out, HIGH);
-      Serial.println(currentMillis - previousMillis);
   }
        
   
